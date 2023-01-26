8eaf2bd193ad31d452b5e765f373a02a8cfdb039
KravetsBohdan <bodyakravets@gmail.com>
2023-01-26 14:31:30 +0200

    change returning type

--- src/main/java/com/bkravets/homework19/controller/StudentController.java
+++ src/main/java/com/bkravets/homework19/controller/StudentController.java
@@ -1,9 +1,12 @@
 package com.bkravets.homework19.controller;
 
+import com.bkravets.homework19.entity.Student;
 import com.bkravets.homework19.service.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/students")
@@ -11,7 +14,7 @@ public class StudentController {
     private final StudentService studentService;
 
     @GetMapping
-    public void getStudents() {
-        studentService.getStudents();
+    public List<Student> getStudents() {
+       return studentService.getStudents();
     }
 }
