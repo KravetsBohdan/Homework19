be0cc2e26e9afa602475d10e1b97b88d979d974a
KravetsBohdan <bodyakravets@gmail.com>
2023-02-01 00:47:47 +0200

    Add StudentService to work with database

--- src/main/java/com/bkravets/homework19/service/StudentService.java
+++ src/main/java/com/bkravets/homework19/service/StudentService.java
@@ -1,7 +1,10 @@
 package com.bkravets.homework19.service;
 
+import com.bkravets.homework19.dto.StudentDTO;
 import com.bkravets.homework19.entity.Student;
+import com.bkravets.homework19.mapper.StudentMapper;
 import com.bkravets.homework19.repository.StudentRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,17 +16,44 @@ public class StudentService {
 
     private final StudentRepository studentRepository;
 
-    public void createStudent(String name, String email) {
+    public List<StudentDTO> getStudents() {
+        return studentRepository.findAll()
+                .stream()
+                .map(StudentMapper.INSTANCE::toStudentDTO)
+                .toList();
+    }
 
-        Student student = new Student();
-        student.setName(name);
-        student.setEmail(email);
+    public StudentDTO getStudent(long id) {
+        return studentRepository.findById(id)
+                .map(StudentMapper.INSTANCE::toStudentDTO)
+                .orElseThrow();
+    }
 
+
+    public StudentDTO createStudent(StudentDTO studentDTO) {
+        Student student = StudentMapper.INSTANCE.toStudent(studentDTO);
         studentRepository.save(student);
+        return studentDTO;
     }
 
-    public List<Student> getStudents() {
-        return studentRepository.findAll();
+    @Transactional
+    public StudentDTO updateStudent(long id, StudentDTO studentDTO) {
+        Student student = studentRepository.findById(id).orElseThrow();
+        student.setName(studentDTO.getName());
+        student.setEmail(studentDTO.getEmail());
+        studentRepository.save(student);
+
+        return studentDTO;
     }
+
+    @Transactional
+    public void deleteStudent(long id) {
+        if (!studentRepository.existsById(id)) {
+            throw new IllegalArgumentException("Student not found");
+        }
+
+        studentRepository.deleteById(id);
+    }
+
 
 }
