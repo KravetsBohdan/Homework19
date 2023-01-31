b58181f07ee084b98c92b0c012408c508b785063
KravetsBohdan <bodyakravets@gmail.com>
2023-02-01 00:48:13 +0200

    Add StudentService with basic endpoints

--- src/main/java/com/bkravets/homework19/controller/StudentController.java
+++ src/main/java/com/bkravets/homework19/controller/StudentController.java
@@ -1,11 +1,11 @@
 package com.bkravets.homework19.controller;
 
-import com.bkravets.homework19.entity.Student;
+import com.bkravets.homework19.dto.StudentDTO;
 import com.bkravets.homework19.service.StudentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,7 +16,29 @@ public class StudentController {
     private final StudentService studentService;
 
     @GetMapping
-    public List<Student> getStudents() {
-       return studentService.getStudents();
+    public ResponseEntity<List<StudentDTO>> getStudents() {
+        return new ResponseEntity<>(studentService.getStudents(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<StudentDTO> getStudent(@PathVariable long id) {
+        return new ResponseEntity<>(studentService.getStudent(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<StudentDTO> createStudent(@RequestBody StudentDTO studentDTO) {
+        return new ResponseEntity<>(studentService.createStudent(studentDTO), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<StudentDTO> updateStudent(@PathVariable long id,
+                                                    @RequestBody StudentDTO studentDTO) {
+        return new ResponseEntity<>(studentService.updateStudent(id, studentDTO), HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteStudent(@PathVariable long id) {
+        studentService.deleteStudent(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
