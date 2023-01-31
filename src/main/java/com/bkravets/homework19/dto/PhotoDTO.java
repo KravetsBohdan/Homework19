86bfadcfb137d39247cc01c17bbf6e22f5fd0273
KravetsBohdan <bodyakravets@gmail.com>
2023-02-01 00:41:50 +0200

    Add PhotoDTO;Add StudentDTO

---
+++ src/main/java/com/bkravets/homework19/dto/PhotoDTO.java
@@ -0,0 +1,12 @@
+package com.bkravets.homework19.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PhotoDTO {
+
+    private String description;
+    private String url;
+}
---
+++ src/main/java/com/bkravets/homework19/dto/StudentDTO.java
@@ -0,0 +1,13 @@
+package com.bkravets.homework19.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class StudentDTO {
+
+    private String name;
+    private String email;
+}
