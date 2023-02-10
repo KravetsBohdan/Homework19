f9b2ee1dc50dbe9cd3e228e8e281bd495778a479
KravetsBohdan <bodyakravets@gmail.com>
2023-02-10 14:09:40 +0200

    add constructors, equals and hashcode to StudentDto

--- src/main/java/com/bkravets/homework19/dto/StudentDto.java
+++ src/main/java/com/bkravets/homework19/dto/StudentDto.java
@@ -1,11 +1,13 @@
 package com.bkravets.homework19.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 
 @Getter
 @Setter
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudentDto {
 
     private String name;
